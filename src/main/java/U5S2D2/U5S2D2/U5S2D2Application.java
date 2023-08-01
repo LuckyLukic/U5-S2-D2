75931878cdcc763915299758f134a77bd3f9a841
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-08-01 14:38:32 +0200

    base

---
+++ src/main/java/U5S2D2/U5S2D2/U5S2D2Application.java
@@ -0,0 +1,13 @@
+package U5S2D2.U5S2D2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S2D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S2D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/U5S2D2/U5S2D2/U5S2D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package U5S2D2.U5S2D2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S2D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
